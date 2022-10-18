fef9e61fc64e8e56b0481470d14e42275b8f9200
hugoalh <32359235+hugoalh@users.noreply.github.com>
2022-10-18 17:11:47 +0800

    20221018C

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { Chalk } from "chalk";
-import { endGroup as ghactionsEndGroup, error as ghactionsError, getBooleanInput as ghactionsGetBooleanInput, getInput as ghactionsGetInput, info as ghactionsInformation, setOutput as ghactionsSetOutput, setSecret as ghactionsSetSecret, startGroup as ghactionsStartGroup, warning as ghactionsWarning } from "@actions/core";
+import { endGroup as ghactionsEndGroup, error as ghactionsError, getBooleanInput as ghactionsGetBooleanInput, getInput as ghactionsGetInput, setOutput as ghactionsSetOutput, setSecret as ghactionsSetSecret, startGroup as ghactionsStartGroup, warning as ghactionsWarning } from "@actions/core";
 import { isJSON as adIsJSON, isString as adIsString, isStringifyJSON as adIsStringifyJSON } from "@hugoalh/advanced-determine";
 import nodeFetch from "node-fetch";
 import yaml from "yaml";
-const ghactionsChalk = new Chalk({ level: 3 });
-const iftttMakerURLRegExp = /^https:\/\/maker\.ifttt\.com\/use\/(?<key>(?:[\da-zA-Z][\da-zA-Z_-]*)?[\da-zA-Z])$/gu;
 try {
+	const chalk = new Chalk({ level: 3 });
+	const iftttMakerURLRegExp = /^https:\/\/maker\.ifttt\.com\/use\/(?<key>(?:[\da-zA-Z][\da-zA-Z_-]*)?[\da-zA-Z])$/gu;
 	ghactionsStartGroup(`Import inputs.`);
 	let eventName = ghactionsGetInput("eventname");
 	if (!adIsString(eventName, { pattern: /^(?:[\da-zA-Z][\da-zA-Z_-]*)?[\da-zA-Z]$/gu })) {
@@ -14,7 +14,7 @@ try {
 	if (!adIsString(eventName, { lowerCase: true })) {
 		ghactionsWarning(`Input \`eventname\`'s value \`${eventName}\` is recommended to keep in lower case to prevent issues!`);
 	}
-	ghactionsInformation(`${ghactionsChalk.bold("Event Name:")} ${eventName}`);
+	console.log(`${chalk.bold("Event Name:")} ${eventName}`);
 	let keyRaw = ghactionsGetInput("key");
 	if (!adIsString(keyRaw, { pattern: /^(?:https:\/\/maker\.ifttt\.com\/use\/)?(?:[\da-zA-Z][\da-zA-Z_-]*)?[\da-zA-Z]$/gu })) {
 		throw new TypeError(`Input \`key\` is not a valid IFTTT webhook key!`);
@@ -41,7 +41,7 @@ try {
 		throw new TypeError(`\`${payload}\` is not a valid IFTTT webhook JSON/YAML/YML payload!`);
 	}
 	let payloadStringify = JSON.stringify(payload);
-	ghactionsInformation(`${ghactionsChalk.bold("Payload:")} ${payloadStringify}`);
+	console.log(`${chalk.bold("Payload:")} ${payloadStringify}`);
 	ghactionsEndGroup();
 	ghactionsStartGroup(`Post network request to IFTTT.`);
 	let response = await nodeFetch(
@@ -67,8 +67,8 @@ try {
 	if (!response.ok) {
 		throw new Error(`Unexpected response status \`${response.status} ${response.statusText}\`: ${responseText}`);
 	}
-	ghactionsInformation(`${ghactionsChalk.bold("Response Status:")} ${response.status} ${response.statusText}`);
-	ghactionsInformation(`${ghactionsChalk.bold("Response Content:")} ${responseText}`);
+	console.log(`${chalk.bold("Response Status:")} ${response.status} ${response.statusText}`);
+	console.log(`${chalk.bold("Response Content:")} ${responseText}`);
 	ghactionsEndGroup();
 } catch (error) {
 	ghactionsError(error?.message);
