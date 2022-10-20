d381c190922e7b359b426299cea3040044fed722
hugoalh <32359235+hugoalh@users.noreply.github.com>
2022-10-20 13:16:02 +0800

    Optimize bundler

--- bundler.js
+++ bundler.js
@@ -5,12 +5,19 @@ import { promisify } from "node:util";
 import { readdir, rename, rm } from "node:fs/promises";
 const childProcessExec = promisify(exec);
 const outputDirectory = "dist";
-const outputFullPath = pathJoin(dirname(fileURLToPath(import.meta.url)), outputDirectory);
+const outputDirectoryFullPath = pathJoin(dirname(fileURLToPath(import.meta.url)), outputDirectory);
+/* Remove all of the files in the `outputDirectory`, need to await in order to prevent race conditions. */
+for (const outputFile of await readdir(outputDirectoryFullPath, { withFileTypes: true })) {
+	await rm(pathJoin(outputDirectoryFullPath, outputFile.name));
+}
+/* Invoke `ncc` bundler. */
 console.log(await childProcessExec(`ncc build src/main.js --out ${outputDirectory} --no-cache --no-source-map-register --target es2021`));
-for (const outputFile of await readdir(outputFullPath, { withFileTypes: true })) {
+/* Fix `ncc` bundler issues, no need to await due to no race conditions. */
+for (const outputFile of await readdir(outputDirectoryFullPath, { withFileTypes: true })) {
+	const outputFileFullPath = pathJoin(outputDirectoryFullPath, outputFile.name);
 	if (outputFile.name === "index.js") {
-		rename(pathJoin(outputFullPath, outputFile.name), pathJoin(outputFullPath, "main.js"));
+		rename(outputFileFullPath, pathJoin(outputDirectoryFullPath, "main.js"));
 	} else {
-		rm(pathJoin(outputFullPath, outputFile.name));
+		rm(outputFileFullPath);
 	}
 }
